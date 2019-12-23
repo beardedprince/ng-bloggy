c52728c9163374eb1a78a77eb13c87a3b9b81a85
beardedprince <alaketuolamilekan02@gmail.com>
2019-12-23 20:17:03 +0100

    add dists files

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
+import {FormGroup, FormBuilder, Validators} from '@angular/forms';
+import {AuthsService} from '../auths.service';
 
 
 @Component({
@@ -9,9 +11,27 @@ import * as firebase from 'firebase';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent  {
+
+  postForm: FormGroup;
+  submitted = false;
+  success = false;
   user: firebase.User;
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth, private formBuilder: FormBuilder, private authService: AuthsService) {
+    this.postForm = this.formBuilder.group({
+      postTitle: ['', Validators.required],
+      postBody: ['', Validators.required]
+    });
     afAuth.authState.subscribe(user => this.user = user);
+   }
+
+   submitPost() {
+     this.submitted = true;
+     if (this.postForm.invalid) {
+       return;
+     }
+     this.success = true;
+     console.log(this.postForm.value);
+     this.authService.getPost();
    }
 
    logout() {
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase';
+import {AuthsService} from '../auths.service';
 
 @Component({
   selector: 'app-navbar',
@@ -11,10 +12,10 @@ export class NavbarComponent  {
 
   user: firebase.User;
 
-  constructor(private afAuth: AngularFireAuth) {
+  constructor(private afAuth: AngularFireAuth, private authService: AuthsService) {
     afAuth.authState.subscribe(user => this.user = user);
    }
   logout() {
-    this.afAuth.auth.signOut();
+    this.authService.logOutUser();
   }
 }
